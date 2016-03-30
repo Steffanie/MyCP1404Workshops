7f68b04c4bcd8d3e4d3e30aad72320e0976526f5
Steffanie Gozzano <steffanie.gozzano@my.jcu.edu.au>
2016-03-30 18:28:00 +1000

    updated with error checking

---
+++ triangle.py
@@ -0,0 +1,7 @@
+def triangleWorkouterer(base, length):
+    base = float(input("Enter base length: "))
+    height = int(input("Enter height length: "))
+    area = (base * height) / 2
+    return area
+
+print(triangleWorkouterer(0,0))
